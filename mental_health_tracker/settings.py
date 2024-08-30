6c17941aa21ebcdea0495e763ba2e0d206f0e0cf
hubb <hsyadid2005@gmail.com>
2024-08-30 13:54:36 +0700

    setup pws

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -121,4 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","hubban.syadid-mentalhealthtracker.pbp.cs.ui.ac.id"]

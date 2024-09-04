faf9471a3aa016d72c921f26e71a55a4bcc29502
hubb <hsyadid2005@gmail.com>
2024-09-04 13:55:43 +0700

    <pesan_commit>

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","hubban.syadid-mentalhealthtracker.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","hubban.syadid-mentalhealthtracker.pbp.cs.ui.ac.id."]
